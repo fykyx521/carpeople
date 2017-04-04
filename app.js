296971ca2251428fd8eb7e7a450ad91dd209a099
nxl_jsb <sxnxl@nxl-jsbdeMacBook-Pro.local>
2017-04-04 22:39:36 +0800

    没啥

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import login from 'utils/login.js';
 App({
     onLaunch: function () {
         var Bmob = require('utils/bmob.js');
+        Bmob.initialize("d906953dd3d938e25d240485f44c57bd", "e9c5aaa92fd184361486049d4fef2a8c");
         console.log('App Launch')
         
         login.login();
+        
     },
     onShow: function () {
         console.log('App Show')
