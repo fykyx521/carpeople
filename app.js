23994c2e6676015c679a278e6482e269a32f1e67
fanyk <javaflex@yeah.net>
2017-04-05 09:51:51 +0800

    去掉key

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import login from 'utils/login.js';
 App({
     onLaunch: function () {
         var Bmob = require('utils/bmob.js');
-        Bmob.initialize("d906953dd3d938e25d240485f44c57bd", "e9c5aaa92fd184361486049d4fef2a8c");
         console.log('App Launch')
         
         login.login();
